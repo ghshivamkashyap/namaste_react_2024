80ae4176ca088ba16f67e749632ccff30ceacb2a
SHIVAM KASHYAP <lootforecast@gmail.com>
2024-10-02 23:25:30 +0530

    parcel build working with react

--- class 2/app.js
+++ class 2/app.js
@@ -1,1 +1,11 @@
-console.log("Hello word");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading = React.createElement(
+  "h1",
+  { id: "i_am_id", className: "iAmClasss" },
+
+  "i am h1 dynamic"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
