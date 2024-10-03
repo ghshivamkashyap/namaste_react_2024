12a4ff39dd5ef7fe78c0113316a4e8e8690a204a
SHIVAM KASHYAP <lootforecast@gmail.com>
2024-10-03 22:36:18 +0530

    coe using jsx

--- class 2/app.js
+++ class 2/app.js
@@ -7,5 +7,11 @@ const heading = React.createElement(
 
   "i am h1 dynamic"
 );
+
+const jsxHeading = <h1 className="i_am_class_of_jsx">i am jsx heading made using jsx</h1>;
+
+console.log("jsxHeading: ", jsxHeading);
+console.log("heading: ", heading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
