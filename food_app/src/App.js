01bb371871d70eecc8fce426543d3770b42dff7c
shivam digiprod <lootforecast@gmail.com>
2024-10-04 22:33:02 +0530

    food app started

---
+++ food_app/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import TopNavbar from "./components/TopNavbar";
+
+function App() {
+  return (
+    <div className="flex flex-col">
+      <TopNavbar />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ food_app/src/components/TopNavbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TopNavbar = () => {
+  return (
+    <ul className=" flex gap-x-5">
+      <li>contact</li>
+      <li>contact</li>
+      <li>contact</li>
+      <li>contact</li>
+      <li>contact</li>
+    </ul>
+  );
+};
+
+export default TopNavbar;
