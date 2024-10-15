4332add21d9224b2a3d4c898400c140b4598e87e
SHIVAM KASHYAP <lootforecast@gmail.com>
2024-10-15 18:51:55 +0530

    state variable in class component

--- food_app/src/components/class_and_functionsl_component/ClassComp.js
+++ food_app/src/components/class_and_functionsl_component/ClassComp.js
@@ -4,6 +4,10 @@ class ClassComp extends React.Component {
   constructor(props) {
     super(props);
     console.log("Class component props: ", props);
+
+    this.state = {
+      count: 0,
+    };
   }
   render() {
     return (
@@ -12,6 +16,7 @@ class ClassComp extends React.Component {
           i am class component{" "}
           <span className="  bg-slate-400">{this.props?.data}</span>
         </div>
+        <p>{this.state.count+1}</p>
       </>
     );
   }
