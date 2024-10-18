015412f3feb2bff30ac252af654c688a03e186e9
shivam digiprod <lootforecast@gmail.com>
2024-10-18 19:01:33 +0530

    online offline status custom hook done

--- food_app/src/components/TopNavbar.jsx
+++ food_app/src/components/TopNavbar.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import useOnlineStatus from "../utils/customHooks/useOnlineStatus";
 
 export const TopNavbar = () => {
+  const status = useOnlineStatus();
   return (
     <ul className=" flex gap-x-5 ">
+      <div
+        class={`h-3 w-3 ${
+          status == true ? "bg-green-500" : "bg-red-500"
+        } rounded-full mt-2 ml-2`}
+      ></div>
+
       <li>contact</li>
       <li>orders</li>
       <li>restorants</li>
