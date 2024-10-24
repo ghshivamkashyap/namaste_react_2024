36074e43245cc5394c27b1c124be8202e71ba25f
shivam digiprod <lootforecast@gmail.com>
2024-10-24 20:23:39 +0530

    create slice done for store

---
+++ food_app/src/store/slices/cartSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: { items: [] },
+  reducers: {
+    addItem: (state, action) => {
+      state.items.push(action.payload);
+    },
+    removeItem: (state, action) => {
+      state.items.pop();
+    },
+    emptyCart: (state, action) => {
+      state.items.length = 0;
+    },
+  },
+});
+
+export const { addItem, emptyCart, removeItem } = cartSlice.actions;
+export default cartSlice.reducer;
