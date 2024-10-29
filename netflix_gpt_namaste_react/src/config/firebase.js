0d64e9ab3ccbb41b5c957955f1dd23fc6c9f041f
SHIVAM KASHYAP <lootforecast@gmail.com>
2024-10-29 23:18:10 +0530

    ev setup done

--- netflix_gpt_namaste_react/src/config/firebase.js
+++ netflix_gpt_namaste_react/src/config/firebase.js
@@ -1,16 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyCyzPr9IMHmpzkXmpPKC9cF6evPqvaC7gc",
-  authDomain: "netflixgpt-34ea0.firebaseapp.com",
-  projectId: "netflixgpt-34ea0",
-  storageBucket: "netflixgpt-34ea0.appspot.com",
-  messagingSenderId: "453622124467",
-  appId: "1:453622124467:web:c8a42860648dc22a1185c6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
