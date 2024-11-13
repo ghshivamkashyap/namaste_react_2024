d2542f699af0b87831ab3171ac9f413014ce417c
SHIVAM KASHYAP <lootforecast@gmail.com>
2024-11-13 21:31:11 +0530

    changes

--- netflix_gpt_namaste_react/src/utils/openAi.js
+++ netflix_gpt_namaste_react/src/utils/openAi.js
@@ -3,7 +3,7 @@ import OpenAI from "openai";
 const client = new OpenAI({
   //   apiKey: process.env.OPENAI_API_KEY,
   apiKey:
-    "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",
+    "",
   dangerouslyAllowBrowser: true,
 });
 
