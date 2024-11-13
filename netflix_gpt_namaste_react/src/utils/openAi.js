06aa629e2faae2ae3348c8340bc73b73905e640c
SHIVAM KASHYAP <lootforecast@gmail.com>
2024-11-13 21:32:17 +0530

    changes

--- netflix_gpt_namaste_react/src/utils/openAi.js
+++ netflix_gpt_namaste_react/src/utils/openAi.js
@@ -1,10 +1,7 @@
 import OpenAI from "openai";
 
 const client = new OpenAI({
-  //   apiKey: process.env.OPENAI_API_KEY,
-  apiKey:
-    "",
-  dangerouslyAllowBrowser: true,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 export default client;
